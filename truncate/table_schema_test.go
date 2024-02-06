f9859a1e18b6ff6853ac3ccd63978db961db8ae8
Takuya Shuto <shuto.takuya.sy@alumni.tsukuba.ac.jp>
2024-02-06 23:31:33 +0900

    Fix TestFilterTableSchemas

--- truncate/table_schema_test.go
+++ truncate/table_schema_test.go
@@ -17,104 +17,103 @@
 package truncate
 
 import (
-	"reflect"
-	"sort"
+	"github.com/google/go-cmp/cmp"
+	"github.com/google/go-cmp/cmp/cmpopts"
 	"testing"
 )
 
 func TestFilterTableSchemas(t *testing.T) {
-	t.Parallel()
+	var (
+		// The following tables are hierarchical schemas.
+		// The table schemas are well known in Cloud Spanner document about 'schema and data model'.
+		singers = &tableSchema{
+			tableName:            "Singers",
+			parentTableName:      "",
+			parentOnDeleteAction: deleteActionUndefined,
+			referencedBy:         nil,
+		}
+		albums = &tableSchema{
+			tableName:            "Albums",
+			parentTableName:      "Singers",
+			parentOnDeleteAction: deleteActionCascadeDelete,
+			referencedBy:         nil,
+		}
+		songs = &tableSchema{
+			tableName:            "Songs",
+			parentTableName:      "Albums",
+			parentOnDeleteAction: deleteActionCascadeDelete,
+			referencedBy:         nil,
+		}
 
-	testCases := map[string]struct {
+		// The following tables are flat schemas and not related to each other.
+		t1 = &tableSchema{
+			tableName:            "t1",
+			parentTableName:      "",
+			parentOnDeleteAction: deleteActionUndefined,
+			referencedBy:         nil,
+		}
+		t2 = &tableSchema{
+			tableName:            "t2",
+			parentTableName:      "",
+			parentOnDeleteAction: deleteActionUndefined,
+			referencedBy:         nil,
+		}
+		t3 = &tableSchema{
+			tableName:            "t3",
+			parentTableName:      "",
+			parentOnDeleteAction: deleteActionUndefined,
+			referencedBy:         nil,
+		}
+	)
+
+	for _, test := range []struct {
+		desc          string
 		schemas       []*tableSchema
 		excludeTables []string
 		want          []*tableSchema
 	}{
-		"Exclude the parent tables by tracing up to the topmost level.": {
+		{
+			desc:          "Exclude the parent tables by tracing up to the topmost level.",
 			schemas:       []*tableSchema{singers, albums, songs, t1, t2, t3},
 			excludeTables: []string{songs.tableName},
 			want:          []*tableSchema{t1, t2, t3},
 		},
-		"Exclude only the higher levels without the lower levels.": {
+		{
+			desc:          "Exclude only the higher levels without the lower levels.",
 			schemas:       []*tableSchema{singers, albums, songs, t1, t2, t3},
 			excludeTables: []string{albums.tableName},
 			want:          []*tableSchema{songs, t1, t2, t3},
 		},
-		"Exclude multiple tables.": {
+		{
+			desc:          "Exclude multiple tables.",
 			schemas:       []*tableSchema{singers, albums, songs, t1, t2, t3},
 			excludeTables: []string{songs.tableName, t1.tableName, t2.tableName},
 			want:          []*tableSchema{t3},
 		},
-		"Do nothing when no exclude tables are passed.": {
+		{
+			desc:          "Do nothing when no exclude tables are passed.",
 			schemas:       []*tableSchema{singers, albums, songs, t1, t2, t3},
 			excludeTables: nil,
 			want:          []*tableSchema{singers, albums, songs, t1, t2, t3},
 		},
-	}
+	} {
+		t.Run(test.desc, func(t *testing.T) {
+			got := filterTableSchemas(test.schemas, test.excludeTables)
 
-	for name, tc := range testCases {
-		tc := tc
-		t.Run(name, func(t *testing.T) {
-			t.Parallel()
-
-			got := filterTableSchemas(tc.schemas, tc.excludeTables)
-
-			if len(got) != len(tc.want) {
-				t.Errorf("len(got) %d, len(want) %d", len(got), len(tc.want))
+			if len(got) != len(test.want) {
+				t.Errorf("len(got) %d, len(want) %d", len(got), len(test.want))
 			}
 
-			sort.Slice(got, func(i, j int) bool {
-				return got[i].tableName < got[j].tableName
-			})
-			sort.Slice(tc.want, func(i, j int) bool {
-				return tc.want[i].tableName < tc.want[j].tableName
-			})
+			opts := []cmp.Option{
+				cmpopts.SortSlices(func(i, j *tableSchema) bool {
+					return i.tableName < j.tableName
+				}),
+				cmp.AllowUnexported(tableSchema{}),
+			}
 
-			for i, w := range tc.want {
-				if !reflect.DeepEqual(*got[i], *w) {
-					t.Errorf("got %v, want %v", got[i], w)
-				}
+			if diff := cmp.Diff(got, test.want, opts...); diff != "" {
+				t.Errorf("mismatch (-got +want):\n%s", diff)
 			}
 		})
 	}
 }
-
-// The table schemas are well known in Cloud Spanner document about 'schema and data model'.
-var (
-	singers = &tableSchema{
-		tableName:            "Singers",
-		parentTableName:      "",
-		parentOnDeleteAction: deleteActionUndefined,
-		referencedBy:         nil,
-	}
-	albums = &tableSchema{
-		tableName:            "Albums",
-		parentTableName:      "Singers",
-		parentOnDeleteAction: deleteActionCascadeDelete,
-		referencedBy:         nil,
-	}
-	songs = &tableSchema{
-		tableName:            "Songs",
-		parentTableName:      "Albums",
-		parentOnDeleteAction: deleteActionCascadeDelete,
-		referencedBy:         nil,
-	}
-	t1 = &tableSchema{
-		tableName:            "t1",
-		parentTableName:      "",
-		parentOnDeleteAction: deleteActionUndefined,
-		referencedBy:         nil,
-	}
-	t2 = &tableSchema{
-		tableName:            "t2",
-		parentTableName:      "",
-		parentOnDeleteAction: deleteActionUndefined,
-		referencedBy:         nil,
-	}
-	t3 = &tableSchema{
-		tableName:            "t3",
-		parentTableName:      "",
-		parentOnDeleteAction: deleteActionUndefined,
-		referencedBy:         nil,
-	}
-)
